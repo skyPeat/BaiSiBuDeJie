8ee5dfe910f97dc625c151d31d6130a0aeb3d7ef
PTF <ptf@163.com>
2016-11-21 19:48:45 +0800

    创建项目

---
+++ 百思不得姐/百思不得姐/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  百思不得姐
+//
+//  Created by 潘天峰 on 16/11/21.
+//  Copyright © 2016年 skyPeat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
